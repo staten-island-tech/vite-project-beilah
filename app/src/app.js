1190b51b3d5a24c95e2860b540b080bdf74ea21f
Beila <beilah@nycstudents.net>
2025-11-25 14:02:54 -0500

    ass

--- app/src/app.js
+++ app/src/app.js
@@ -5,21 +5,21 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "Howl's Moving Castle",
     length: "5:11",
-    image: "src/1.jpg",
+    image: "./1.jpg",
   },
   {
     name: "The Flower Garden",
     artist: "Joe Hisaishi",
     album: "Howl's Moving Castle",
     length: "2:58",
-    image: "src/1.jpg",
+    image: "./1.jpg",
   },
   {
     name: "Opening Song - Merry-Go-Round of Life",
     artist: "Joe Hisaishi",
     album: "Howl's Moving Castle Soundtrack",
     length: "2:33",
-    image: "src/1.jpg",
+    image: "./1.jpg",
   },
 
   // Kiki's Delivery Service
@@ -28,21 +28,21 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "Kiki's Delivery Service",
     length: "3:00",
-    image: "src/2.jpg",
+    image: "./2.jpg",
   },
   {
     name: "On a Clear Day",
     artist: "Joe Hisaishi",
     album: "Kiki's Delivery Service",
     length: "2:16",
-    image: "src/2.jpg",
+    image: "./2.jpg",
   },
   {
     name: "A Town with an Ocean View",
     artist: "Joe Hisaishi",
     album: "Kiki's Delivery Service",
     length: "3:58",
-    image: "src/2.jpg",
+    image: "./2.jpg",
   },
 
   // Laputa: Castle in the Sky
@@ -51,14 +51,14 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "Laputa: Castle in the Sky",
     length: "2:27",
-    image: "src/3.jpg",
+    image: "./3.jpg",
   },
   {
     name: "Castle in the Sky",
     artist: "Joe Hisaishi",
     album: "Laputa: Castle in the Sky",
     length: "4:37",
-    image: "src/3.jpg",
+    image: "./3.jpg",
   },
 
   // My Neighbor Totoro
@@ -67,7 +67,7 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "My Neighbor Totoro",
     length: "3:42",
-    image: "src/4.jpg",
+    image: "./4.jpg",
   },
 
   // Princess Mononoke
@@ -76,7 +76,7 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "Princess Mononoke",
     length: "4:39",
-    image: "src/5.jpg",
+    image: "./5.jpg",
   },
 
   // Spirited Away
@@ -85,7 +85,7 @@ const songs = [
     artist: "Joe Hisaishi",
     album: "Spirited Away",
     length: "3:09",
-    image: "src/6.jpg",
+    image: "./6.jpg",
   },
 
   // Nostalgia ~ Piano Stories II
@@ -94,14 +94,14 @@ const songs = [
     artist: "Joe Hisaishi, Orchestra Città di Ferrara",
     album: "NOSTALGIA~PIANO STORIES II~",
     length: "4:52",
-    image: "src/7.jpg",
+    image: "./7.jpg",
   },
   {
     name: "Nostalgia",
     artist: "Joe Hisaishi, Orchestra Città di Ferrara",
     album: "NOSTALGIA~PIANO STORIES II~",
     length: "3:33",
-    image: "src/7.jpg",
+    image: "./7.jpg",
   },
 ];
 
@@ -120,4 +120,34 @@ songs.forEach((song) => {
   container.innerHTML += html;
 });
 
-console.log("hailey succs");
+const createPlaylist = document.querySelector("#createPlaylist");
+createPlaylist.addEventListener("click", function () {
+  container.innerHTML = "";
+  const form = document.querySelector(".form");
+  form.classList.remove("hidden");
+})
+
+
+
+
+
+
+
+const all = document.querySelector("#viewBtn");
+
+all.addEventListener("click", function () {
+  container.innerHTML = "";
+
+  songs.forEach((song) => {
+    const html = `
+          <div class="card" data-name='${song.name}' >
+              <h2 class="cardHeader">${song.name}</h2>\
+              <h4 class="album">${song.album}</h4>
+              <img src="${song.image}" class="img"/>
+              <h3 class="artist">${song.artist}</h3>
+              <h5 class="length">${song.length}</h5>
+          </div>
+          `;
+    container.innerHTML += html;
+  })
+})
